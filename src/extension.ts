62ae5d649e67f7cd83572e9cac81bd7b0b5bac87
Christof Marti <chrmarti@microsoft.com>
2021-09-28 14:32:14 +0200

    test-platform-specific-extensions2

--- src/extension.ts
+++ src/extension.ts
@@ -8,15 +8,15 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-platform-specific-extensions" is now active!');
+	console.log('Congratulations, your extension "test-platform-specific-extensions2" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-platform-specific-extensions.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('test-platform-specific-extensions2.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello linux-x64!');
+		vscode.window.showInformationMessage('Hello darwin-x64!');
 	});
 
 	context.subscriptions.push(disposable);
